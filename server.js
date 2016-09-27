90018a2a35effb87d7f68d2f24cb868c33fb4553
ganeshmukesh123 <ganeshmukesh123@gmail.com>
2016-09-28 01:21:16 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.get('/', function (req, res) {
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
-app.get('/article_one.html', function (req, res) {
+app.get('/article_one', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'article_one.html'));
 });
 app.get('/ui/madi.png', function (req, res) {
