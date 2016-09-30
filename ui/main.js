e8cc295dc8d36f40f2cc226be01b5aec15197394
ganeshmukesh123 <ganeshmukesh123@gmail.com>
2016-09-30 07:24:20 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -5,10 +5,10 @@ element.innerHTML='NEW Value';
 var img=document.getElementById('madi');
 var marginLeft=10;
 function moveRight(){
-    marginLeft= marginLeft + 10;
+    marginLeft= marginLeft + 5;
     img.style.marginLeft = marginLeft + 'px';
 }
 img.onclick=function(){
-    var interval = setInterval(moveRight,100);
+    var interval = setInterval(moveRight,50);
     
 };
