4b0da034cd28a5906d554c83369b37f4324cb846
ganeshmukesh123 <ganeshmukesh123@gmail.com>
2016-10-10 18:31:22 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -22,8 +22,20 @@ img.onclick=function(){
 var button=document.getElementById("counter");
 var counter=0;
 button.onclick= function(){
-    
-    counter=counter+1;
-    var span=document.getElementById("count");
-    span.innerHTML=counter.toString();
-}
+    //Create requestobject
+    var request= new XMLHttpRequest();
+    //Capture the response and store it in a variable
+    request.onreadystatechange=function(){
+        if(request.readyState===XMLHttpRequest.DONE){
+            if(request.status===200){
+                var counter = request.responseText;
+                var span=document.getElementById("count");
+                span.innerHTML=counter.toString();
+            
+            }
+        }
+    };
+    //make request
+    request.open('GET','http://ganeshmukesh123.imad.hasura-app.io/counter',true);
+    request.send(null);
+};
